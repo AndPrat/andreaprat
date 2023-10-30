eebc4e1c563a9d34003f7f9f133e5a6e9cf7df6c
Andrea Prat <andr8ide@gmail.com>
2023-10-30 14:03:25 +0100

    Create LazyAboutPage and add in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import NavigationBar from "../NavigationBar/NavigationBar";
+import paths from "../../routers/paths/paths";
+import { Suspense } from "react";
+import { LazyAboutPage } from "../../pages/LazyPages/LazyPages";
 
 const App = (): React.ReactElement => {
   return (
     <div className="container">
       <Header />
       <NavigationBar />
+      <Routes>
+        <Route
+          path={paths.aboutPage}
+          element={
+            <Suspense>
+              <LazyAboutPage />
+            </Suspense>
+          }
+        />
+      </Routes>
     </div>
   );
 };
