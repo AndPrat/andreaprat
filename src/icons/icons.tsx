8336dab9e6308aba02306f49d058398640bae6c4
Andrea Prat <andr8ide@gmail.com>
2023-11-05 18:11:27 +0100

    Add other section, main skills, icon and image in AboutPage

--- src/icons/icons.tsx
+++ src/icons/icons.tsx
@@ -133,6 +133,25 @@ const iconHeart = (
   </svg>
 );
 
+const iconDiamond = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-diamond"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M6 5h12l3 5l-8.5 9.5a.7 .7 0 0 1 -1 0l-8.5 -9.5l3 -5" />
+    <path d="M10 12l-2 -2.2l.6 -1" />
+  </svg>
+);
+
 export {
   iconTwitter,
   iconFacebook,
@@ -141,4 +160,5 @@ export {
   iconInstagram,
   iconGithub,
   iconHeart,
+  iconDiamond,
 };
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,18 +1,54 @@
+import { Helmet } from "react-helmet";
 import "./AboutPage.css";
+import { iconDiamond } from "../../icons/icons";
 
 const AboutPage = (): React.ReactElement => {
   return (
     <>
-      <meta charSet="utf-8" />
-      <title>About</title>
-      <meta name="description" content="About Andrea Prat" />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>About</title>
+        <meta name="description" content="About Andrea Prat" />
+      </Helmet>
       <div className="about-page">
-        <div className="container">
-          <h2 className="about-page__title">About me</h2>
-          <p className="about-page__information">
-            {`Hello! I'm Andrea Prat, a creative frontend developer & graphic desing
+        <div className="container container--flex about-page__container">
+          <img
+            src="/images/Andrea-blue.png"
+            alt="Andrea Polaroiod photos"
+            width={250}
+            className="about-page__photo"
+          />
+          <div className="about-page__block">
+            <div className="about-page__block--mini">
+              <h2 className="about-page__title">About me</h2>
+              <p className="about-page__information">
+                {`Hello! I'm Andrea Prat, a creative frontend developer & graphic desing
           and illustrator w/ a focus on creative project design.`}
+              </p>
+            </div>
+            <div className="about-page__block--mini">
+              <h2 className="about-page__title-secction">
+                We are a match if you ...
+              </h2>
+              <p className="about-page__information">
+                Look for a designer with a broad skill set ranging from
+                developer web to graphic design & illustration.
+              </p>
+              <button className="button button__gradient">{`Let's talk!`}</button>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <h4 className="about-page__title-important">
+            {iconDiamond} Main skills
+          </h4>
+          <p className="about-page__information-important">
+            React.js • Redux.js • TypeScript • Unit Test • Graphic design •
+            Adobe Package • Procreate
           </p>
+        </div>
+        <div className="container">
+          <h3 className="about-page__title-secction-secundary">Mini CV</h3>
         </div>
       </div>
     </>
