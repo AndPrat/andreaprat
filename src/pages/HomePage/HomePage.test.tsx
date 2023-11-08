96f86894e32216b7901f8eb5456a7637d6f6f800
Andrea Prat <andr8ide@gmail.com>
2023-11-08 11:41:40 +0100

    Test HomePage component

---
+++ src/pages/HomePage/HomePage.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+
+describe("Given the HomePage component", () => {
+  describe("When it is rendered", () => {
+    test("Then it show should a headings with the texts 'G'day! I'm Andrea', 'Some of my latest work' and 'Personal projects'", () => {
+      const expectedHeadingOne = "G'day! I'm Andrea";
+      const expectedHeadingTwo = "Some of my latest work";
+      const expectedHeadingThree = "Personal projects";
+
+      render(
+        <BrowserRouter>
+          <HomePage />
+        </BrowserRouter>,
+      );
+
+      const headingOne = screen.getByRole("heading", {
+        name: expectedHeadingOne,
+      });
+      const headingTwo = screen.getByRole("heading", {
+        name: expectedHeadingTwo,
+      });
+      const headingThree = screen.getByRole("heading", {
+        name: expectedHeadingThree,
+      });
+
+      expect(headingOne).toBeInTheDocument();
+      expect(headingTwo).toBeInTheDocument();
+      expect(headingThree).toBeInTheDocument();
+    });
+  });
+});
