415df8da730d4e5c2689873d9b13d245a866401a
Andrea Prat <andr8ide@gmail.com>
2023-11-15 16:00:17 +0100

    Create PortfolioPage

---
+++ src/pages/PortfolioPage/PortfolioPage.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from "react-helmet";
+import "./PortfolioPage.css";
+
+const PorfolioPage = (): React.ReactElement => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Portfolio</title>
+        <meta name="description" content="Ilustration and design Portfolio" />
+      </Helmet>
+      <div className="portfolio-page">
+        <div className="container container--flex portfolio-page__container">
+          <div className="home-page__block">
+            <h2 className="portfolio-page__title">Illustration and Design</h2>
+            <h3 className="portfolio-page__title-secction">Portfolio</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PorfolioPage;
