63392f71d7d11337e8582fd64a13415271df75b8
Andrea Prat <andr8ide@gmail.com>
2024-04-28 20:02:52 +0200

    Create content and gradient styles to fix Header and add gradient line

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,13 @@ import "./Header.css";
 const Header = (): React.ReactElement => {
   return (
     <header className="header">
-      <a href={paths.homePage}>
-        <h1 className="header__text">Andrea Prat</h1>
-      </a>
-      <NavigationBar />
+      <div className="header--gradient"></div>
+      <div className="header__content">
+        <a href={paths.homePage}>
+          <h1 className="header__text">Andrea Prat</h1>
+        </a>
+        <NavigationBar />
+      </div>
     </header>
   );
 };
