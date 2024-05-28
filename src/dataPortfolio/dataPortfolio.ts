0302e501f4db92a61d79ec68e7ad494277eff11f
Andrea Prat <andr8ide@gmail.com>
2024-05-28 15:02:47 +0200

    Create dataPortfolio data and dataPortfolioTypes types

---
+++ src/dataPortfolio/dataPortfolio.ts
@@ -0,0 +1,82 @@
+import { PortfolioData } from "./dataPortfolioTypes";
+
+export const ditifet: PortfolioData = {
+  name: "Dit&Fet!",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const liceuBarcelona: PortfolioData = {
+  name: "Liceu de Barcelona",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const cascanueces: PortfolioData = {
+  name: "El Cascauneces",
+  description: "",
+  category: ["poster"],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const lighterMonster: PortfolioData = {
+  name: "Lighter Mönster",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const menstruation: PortfolioData = {
+  name: "Dit&Fet!",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const taichi: PortfolioData = {
+  name: "Viure amb Taitxí!",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const cantirs: PortfolioData = {
+  name: "Càntirarires",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const vesaliusBeer: PortfolioData = {
+  name: "Vesalius Beer",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
+
+export const nomadic: PortfolioData = {
+  name: "Nomadic!",
+  description: "",
+  category: [],
+  Coverimage: "",
+  image1: "",
+  image2: "",
+};
