574ec4fbb68d088264dce4d47f82a8460f62cd8e
Andrea Prat <andr8ide@gmail.com>
2024-05-28 14:26:26 +0200

    Move the Day component to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,24 @@
+import Marquee from "react-fast-marquee";
 import paths from "../../routers/paths/paths";
 import NavigationBar from "../NavigationBar/NavigationBar";
 import "./Header.css";
+import Day from "../Day/Day";
 
 const Header = (): React.ReactElement => {
   return (
     <header className="header">
-      <div className="header--gradient"></div>
+      <div className="container--flex header__container">
+        <div className="header__block">
+          <div className="header__block--mini">
+            <Marquee autoFill={true}>
+              {" "}
+              <Day />
+            </Marquee>
+          </div>
+        </div>
+      </div>
       <div className="header__content">
+        <div className="header--gradient"></div>
         <a href={paths.homePage}>
           <h1 className="header__text">Andrea Prat</h1>
         </a>
