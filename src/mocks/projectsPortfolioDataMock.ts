56fad13667e4ff6790262919a31520f1aeb94216
Andrea Prat <andr8ide@gmail.com>
2024-08-24 12:35:34 +0200

    Test PortfolioList component with projectsPortfolioDataMock mock

---
+++ src/mocks/projectsPortfolioDataMock.ts
@@ -0,0 +1,22 @@
+import { PortfolioData } from "../dataPortfolio/dataPortfolioTypes";
+
+export const portfolioProjectsListMock: PortfolioData[] = [
+  {
+    id: "01",
+    name: "Dit&Fet!",
+    description: "",
+    category: [],
+    Coverimage: "",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "02",
+    name: "Liceu de Barcelona",
+    description: "",
+    category: [],
+    Coverimage: "",
+    image1: "",
+    image2: "",
+  },
+];
