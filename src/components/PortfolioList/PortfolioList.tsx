a933a96d5aff29ef3036f9e15ef99b75202909ba
Andrea Prat <andr8ide@gmail.com>
2024-10-24 13:04:51 +0200

    Add PorffolioCard in PorfolioList and create styles

--- src/components/PortfolioList/PortfolioList.tsx
+++ src/components/PortfolioList/PortfolioList.tsx
@@ -1,15 +1,18 @@
 import { useAppSelector } from "../../store";
+import PortfolioCard from "../PortfolioCard/PortfolioCard";
+import "./PortfolioList.css";
 
 const PortfolioList = (): React.ReactElement => {
   const portfolioProjects = useAppSelector(
     (state) => state.portfolioProjectsState.portfolioProjects,
   );
+
   return (
     <div className="portfoliolist">
       <ul className="portfoliolist__list">
         {portfolioProjects.map((portfolioProject) => (
           <li key={portfolioProject.id} className="portfoliolist__project">
-            <h3 className="portfoliolist__list">{portfolioProject.name}</h3>
+            {<PortfolioCard portfolioProjects={portfolioProject} />}
           </li>
         ))}
       </ul>
