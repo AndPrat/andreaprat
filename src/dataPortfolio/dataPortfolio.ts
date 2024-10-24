c9d7f863efa9ef3d8896517c7f87c07a1c826a10
Andrea Prat <andr8ide@gmail.com>
2024-10-24 13:09:07 +0200

    Add new content in dataPortfolio

--- src/dataPortfolio/dataPortfolio.ts
+++ src/dataPortfolio/dataPortfolio.ts
@@ -1,91 +1,157 @@
-import { PortfolioData } from "./dataPortfolioTypes";
+import { Portfolio } from "./dataPortfolioTypes";
 
-export const ditifet: PortfolioData = {
-  id: "01",
-  name: "Dit&Fet!",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
+const portfolioProjects: Portfolio = [
+  {
+    id: "01",
+    name: "Dit&Fet!",
+    title1: "The Story Behing DIT&FET",
+    description:
+      "DIT&FET! was born from the idea of improving the experience of moving to a new home, especially for young people with limited income or resources who want the process to be as quick and cost-effective as possible. In an era defined by DIY and handmade solutions, the user's involvement is key to achieving these two goals.",
+    title2: "What's included:",
+    otherInformation:
+      "To make it easier, users can purchase a comprehensive moving pack, which includes: step-by-step instructions for a successful move, a code for validation, an organization system, boxes with tape, a marker, stickers for packages and guests, party planning essentials, invitation cards, and an app to streamline the entire process.",
+    category: ["branding, packaging, personal project"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "02",
+    name: "Liceu de Barcelona",
+    title1: "Rebranding of the Liceu in Barcelona",
+    description:
+      "A new approach to understanding this institution: The Liceu has traditionally been seen as elitist, exclusive, and serious. We aim to transform it by giving it a 360º shift towards a more vibrant and eclectic (kitsch) identity. This redesign will consider everything—from the space itself, textiles, and seating, to the dress code of attendees, questioning whether formal black tie is still necessary. We will also rethink audience behavior, reimagining how people experience classical music in this iconic venue.",
+    title2: "Graphic System",
+    otherInformation:
+      "Our proposal is inspired by the architectural plans of the Liceu, extracting basic geometric shapes to serve as the foundation of the visual system. This reflects the structural essence of the building in a modern way. The aesthetic we are aiming for—without being fully kitsch—contrasts sharply with the current formal, black-and-white Liceu. We will introduce bold, vibrant colors that stand out and break away from the traditional formality, inviting a broader audience. The old Liceu: Luxurious, classical, rigid, exclusive... The new Liceu: Colorful, dynamic, approachable, affordable, and welcoming to all.",
+    category: ["branding, packaging, personal project, poster"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "03",
+    name: "El Cascauneces",
+    title1: "Inspired by the Book of Matches",
+    description:
+      "Drawing inspiration from the book - Book of Matches , this project explores an object through photography. The process leads to the creation of a poster that bears no visual resemblance to the original subject. The object chosen for investigation is a cigarette, which transforms into the iconic ballerina from The Nutcracker in the final design.",
+    category: ["poster, personal project,"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "04",
+    name: "Lighter Mönster",
+    title1: "The Creation of the Lighter Monster",
+    description:
+      "The Lighter Monster was born from a study of various animal parts, plants, and everyday objects from the surrounding environment. It is a small dragon, roughly the size of a lighter. As a nocturnal creature, it breathes fire through its nose to light its way at night and to scare off larger animals. This fire-breathing ability is a defensive mechanism that ensures its survival. This dragon is far from an ordinary species. In addition to having reptilian scales, it also has marsupial fur, adopting traits from different animal species. The monster’s body is made from a plastic lighter. Its large, expressive eyes resemble those of an owl. Its fierce mouth and saber-like teeth, used to defend itself from larger predators, are inspired by the Smilodon, an extinct genus of saber-toothed cats. Its arms, covered in reptilian skin, are borrowed from lizards like iguanas, and its strong, long legs come from kangaroos, enabling it to leap great distances. Lastly, its long, striped tail, derived from lemurs, allows it to hang from tree branches. The scales on its back and shoulders are taken from chameleons, providing camouflage and dorsal protection when needed.",
+    title2: "Graphic A Unique Blend of Animal Traits",
+    otherInformation:
+      "This unique blend of different animal parts was the result of the creator's desire to explore various compositions, combining textures and elements to craft a one-of-a-kind character. The animal parts were not chosen randomly. The creator of the Lighter Monster conducted an in-depth study of different species that stood out for their specific features and the functions of their body parts. Examples include nocturnal birds, mythological creatures, reptiles, lemurids, and more.",
+    category: ["illustration, personal project,"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "05",
+    name: "Vesalius Beer",
+    title1: "Vesalius Beer",
+    description:
+      "Vesalius, brewed in Ghent, is a rich, dark chocolate stout with 6.0% alcohol content. The label design features gothic calligraphy, adding a touch of tradition and elegance to the bottle.",
+    category: ["personal project, packaging, calligraphy"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "06",
+    name: "Viure amb Taitxí!",
+    title1: "Tai Chi Philosophy: A Journey to Personal Balance",
+    description:
+      "This project aims to spread awareness of the martial philosophy of Tai Chi, demonstrating how we can integrate the benefits and principles of this ancient art into our daily lives. Whether through the practice and technique of its movements or its deeper philosophical teachings, Tai Chi offers physical, mental, emotional, and spiritual benefits. The ultimate goal is to achieve a constant balance between body, mind, and spirit by internalizing the values, movements, and principles of Tai Chi, enabling us to attain personal well-being and better face the challenges of modern life. The work includes an illustrated publication that metaphorically explains Tai Chi's martial values through Chinese proverbs, an app that guides and teaches users how to live according to Tai Chi principles, and a Tai Chi uniform designed with visible meridians to highlight the internal benefits of practicing this martial art.",
+    title2: "Design and Philosophy in Harmony",
+    otherInformation:
+      "This project uses blue and red, along with white, as the primary colors—representing the original colors of the first yin-yang symbol ever created. By incorporating these colors, we return to the origins of Tai Chi and its symbol, just as we begin to apply its philosophy to our daily lives. The methodology focuses on the philosophy and movements of Tai Chi, testing martial values and morality in relation to our everyday actions. The project emphasizes improving the technique of primary movements, which are directly linked to Tai Chi’s core principles, and enhancing the bodily experience to incorporate them into a daily routine.",
+    category: ["personal project, illustration, poster, objects"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "07",
+    name: "Càntirarires",
+    title1: "Reviving the Popular Càntir Festival in Argentona",
+    description:
+      "The project we present aims to revive the traditional Càntir (water jug) Festival in Argentona. Throughout history, this humble object has become a symbol, an identity, and a source of pride for the town, and we are determined not to let this tradition fade into obscurity. Our goal is to breathe new life into the festival, making it more open, inclusive, and participatory. The activity will engage the public in the celebration, as up until now, the càntirs have mainly been handled by well-known figures. We want to bring the càntir closer to the people, allowing them to participate in its production process. By doing so, we aim to create a globally appealing event that draws both locals and visitors to join in and celebrate the festival.",
+    title2: "Interactive Experience and Public Engagement",
+    otherInformation:
+      "We plan to transform Argentona's public spaces by placing five large wooden càntirs in the streets, encouraging community involvement and free expression. Anyone who visits will have the opportunity to contribute to the design of Argentona's Càntir of the Year. Participants can paint or creatively intervene with the giant càntirs, making the festival an interactive and memorable experience.",
+    category: ["personal project, poster, illustration, events"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "08",
+    name: "Japan Tea",
+    title1: "The Concept Behind the High-Quality Tea Packaging",
+    description:
+      "The main idea behind the packaging of high-quality tea, given as a gift by the current Emperor of Japan, Akihito, during state receptions with foreign heads of state, is that it should hold significant value and serve a purpose beyond simply storing tea. Therefore, I envisioned it as a decorative element, one that could be repurposed for other uses, making it both beautiful and representative of Japanese culture. It is said that having the image of the Eight Immortals in our homes will protect against negative energies and bring blessings such as good health, good fortune, opportunities, and other blessings, especially for those who honor them with offerings of incense, fruits, and flowers. Therefore, I decided that these figures should be the central and representative element of the packaging. In addition to gifting high-quality tea to foreign heads of state, the packaging would also convey good intentions (such as good fortune and opportunities) to all recipients.",
+    title2: "The Tea Varieties and Their Symbolism",
+    otherInformation:
+      "Each tea label will feature information about the specific type of tea and its associated immortal: Sencha (煎茶): Tsao Kuo Chui. Known for its needle-like appearance; Matcha (抹茶): Chun Li Chuan. Powdered tea used in traditional tea ceremonies; Bantcha (番茶): Lu Tung Pin. A daily tea variety; Gyokuro (玉露): Chan Kuo Lao. A refined tea, protected by bamboo to enhance its flavor; Genmaicha (玄米茶): Ho Sien Ku. A special tea made with brown rice; Mugicha (麦茶): Lan Tsai Ho. A caffeine-free tea made from barley; Gabalong: Li Tsai Ho. The best therapeutic tea; Kunkicha: Hang Hsiang Tzu. A very mild tea with a fresh aroma.",
+    category: ["personal project, illustration, packaging"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "09",
+    name: "Nomadic",
+    title1: "Brand Design: Logo Creation and Application",
+    description:
+      "Nomad: A person who moves from place to place without a permanent home. Synonyms: wanderer, traveler, adventurer, wanderlust. Adjective: Nomadic peoples travel from one place to another, never settling in the same location for long periods. A nomadic lifestyle means moving from place to place without a fixed residence. The mountain symbolizes adventure and freedom, while the circle can be interpreted as the rising or setting sun - marking the end of one adventure and the beginning of another.",
+    category: ["personal project, brand, illustration, poster, objects"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "10",
+    name: "Leeds RockFest Posters",
+    title1: "Typography System Design for Leeds RockFest Posters",
+    description:
+      "The project involves the design of a typographic system applied to the posters for Leeds RockFest. The system utilizes CYMK color schemes and the Interstate typeface in various weights. Texts are arranged diagonally, adding a sense of dynamism to the design.",
+    category: ["poster"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+  {
+    id: "11",
+    name: "RHCP Vinil",
+    title1:
+      "Vinyl Design for Red Hot Chili Peppers - Greatest Hits Ever Live in Concert",
+    description:
+      "The design for the Greatest Hits Ever Live in Concert vinyl by the Red Hot Chili Peppers was created using analog materials such as printers and scanners, with minimal digital photo editing.",
+    category: ["poster"],
+    coverimage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZCOz8jBBcr_3kbJNGu4aZiwIrnJy5xiDmA&s",
+    image1: "",
+    image2: "",
+  },
+];
 
-export const liceuBarcelona: PortfolioData = {
-  id: "02",
-  name: "Liceu de Barcelona",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
-
-export const cascanueces: PortfolioData = {
-  id: "03",
-  name: "El Cascauneces",
-  description: "",
-  category: ["poster"],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
-
-export const lighterMonster: PortfolioData = {
-  id: "04",
-  name: "Lighter Mönster",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
-
-export const menstruation: PortfolioData = {
-  id: "05",
-  name: "Dit&Fet!",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
-
-export const taichi: PortfolioData = {
-  id: "06",
-  name: "Viure amb Taitxí!",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
-
-export const cantirs: PortfolioData = {
-  id: "07",
-  name: "Càntirarires",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
-
-export const vesaliusBeer: PortfolioData = {
-  id: "08",
-  name: "Vesalius Beer",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
-
-export const nomadic: PortfolioData = {
-  id: "09",
-  name: "Nomadic!",
-  description: "",
-  category: [],
-  Coverimage: "",
-  image1: "",
-  image2: "",
-};
+export default portfolioProjects;
